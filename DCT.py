5c757123b32e73bc9aaaa0d0b71d0727005c499c
tw_yshuang <tw.yshuang@gmail.com>
2022-01-13 21:37:30 +0800

    [add][mod][adj] add:     DCT.ipynb, for more easy to understand DCT algorithm. mod:     DCT.py, add func.: dct_2d() & idct_2d(), the idea is provide by lunda, make algorithm complete. adj:     DCT.py, adj some annotation.

--- DCT.py
+++ DCT.py
@@ -1,6 +1,63 @@
+import math
 from PIL import Image
 import numpy as np
 from numba import jit
+
+
+@jit(nopython=True)
+def dct_2d(img: np.ndarray, dct_img: np.ndarray):
+    """
+    ## Original 2D-DCT algorithm
+    input
+    `img`: a 2D array content time domain values.
+    `dct_img`: an empty array that shape is same as img, require for numba @jit accelerate.
+
+    output
+    `dct_img`: a 2D array content frequency domain values.
+    """
+
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            for k in range(img.shape[0]):
+                for l in range(img.shape[1]):
+                    if i == 0 and j == 0:
+                        # print('here:(0, 0)')
+                        dct_img[i, j] += 1 / ((img.shape[0] + img.shape[1]) / 2) * img[k][l]
+                    else:
+                        dct_img[i, j] += (
+                            ((1 / img.shape[0]) + (1 / img.shape[1]))
+                            * img[k, l]
+                            * ((1 / math.sqrt(2)) if i == 0 else (math.cos((2 * k + 1) * i * math.pi / (2 * img.shape[0]))))
+                            * ((1 / math.sqrt(2)) if j == 0 else (math.cos((2 * l + 1) * j * math.pi / (2 * img.shape[1]))))
+                        )
+
+    return dct_img
+
+
+@jit(nopython=True)
+def idct_2d(dct_img, idct_img):
+    """
+    ## Original 2D-IDCT algorithm
+    input
+    `dct_img`: a 2D array content frequency domain values.
+    `idct_img`: an empty array that shape is same as dct_img, require for numba @jit accelerate.
+
+    output
+    `idct_img`: a 2D array content time domain values.
+    """
+
+    for i in range(dct_img.shape[0]):
+        for j in range(dct_img.shape[1]):
+            for k in range(dct_img.shape[0]):
+                for l in range(dct_img.shape[1]):
+                    idct_img[i, j] += (
+                        ((1 / dct_img.shape[0]) + (1 / dct_img.shape[1]))
+                        * dct_img[k, l]
+                        * ((1 / math.sqrt(2)) if k == 0 else math.cos((2 * i + 1) * k * math.pi / (2 * dct_img.shape[0])))
+                        * ((1 / math.sqrt(2)) if l == 0 else math.cos((2 * j + 1) * l * math.pi / (2 * dct_img.shape[1])))
+                    )
+
+    return idct_img
 
 
 @jit(nopython=True)
@@ -17,9 +74,9 @@ def dct_1d(seq: np.ndarray):
     for u_idx in range(len(seq)):  # find index of dct_seq
         value = 0.0
         for m_idx, m in enumerate(seq):
-            value += m * np.cos((2 * m_idx + 1) * u_idx * np.pi / (2 * len(seq)))
+            value += m * math.cos((2 * m_idx + 1) * u_idx * math.pi / (2 * len(seq)))
 
-        dct_seq[u_idx] = value * np.sqrt(2 / len(seq)) if u_idx > 0 else value / np.sqrt(len(seq))
+        dct_seq[u_idx] = value * math.sqrt(2 / len(seq)) if u_idx > 0 else value / math.sqrt(len(seq))
     return dct_seq
 
 
@@ -36,8 +93,8 @@ def idct_1d(seq: np.ndarray):
     idct_seq = np.zeros(seq.shape, dtype=np.float64)
     for m_idx in range(len(seq)):
         for u_idx, u in enumerate(seq):
-            value = np.sqrt(2 / len(seq)) if u_idx > 0 else 1 / np.sqrt(len(seq))
-            value *= u * np.cos((2 * m_idx + 1) * u_idx * np.pi / (2 * len(seq)))
+            value = math.sqrt(2 / len(seq)) if u_idx > 0 else 1 / math.sqrt(len(seq))
+            value *= u * math.cos((2 * m_idx + 1) * u_idx * math.pi / (2 * len(seq)))
 
             idct_seq[m_idx] += value
     return idct_seq
@@ -89,7 +146,7 @@ def idct_fast_2d(dct_img: np.ndarray):
 
 def dct_block(img: np.ndarray, block: tuple = (8, 8)):
     """
-    ## Fast 2D-IDCT by block
+    ## Fast 2D-DCT by block
     input
     `img`: a 2D array content time domain values, usually is an image.
     `block`: (row, col)
@@ -149,14 +206,32 @@ def idct_block(dct_img: np.ndarray, block: tuple = (8, 8)):
 
 if __name__ == '__main__':
 
-    paths = ['./images/lena.tif', './images/lena_std.tif']
+    paths = ['./images/lena.tif', './images/lena_std.tif', './images/puppy.jpg']
     for i, path in enumerate(paths):
         filename = path.split('/')[2].split('.')[0]  # find the filename, e.g. lena, child, etc.
-        img = np.array(Image.open(path))
+        transformed_img = np.array(Image.open(path))
+        # =====================================================#
+        # process by basic DCT_2D full image
+        dct_img = np.array(transformed_img, dtype=np.float64)
+        dims = 1 if len(dct_img.shape) == 2 else dct_img.shape[2]
+        for dim in range(dims):
+            dct_dim = dct_img if dims == 1 else dct_img[:, :, dim]  # iterate by dim's sequnce
+            dct_dim[:, :] = dct_2d(dct_dim, np.zeros_like(dct_dim))
+
+        # print(dct_img)
+        Image.fromarray(dct_img.copy().astype(np.uint8)).save(f'./out/original_dct_{filename}.png')
+
+        idct_img = dct_img.copy()
+        for dim in range(dims):
+            idct_dim = idct_img if dims == 1 else idct_img[:, :, dim]  # iterate by dim's sequnce
+            idct_dim[:, :] = idct_2d(idct_dim, np.zeros_like(idct_dim))
+
+        # print(idct_img)
+        Image.fromarray(idct_img.astype(np.uint8)).save(f'./out/original_idct_{filename}.png')
 
         # =====================================================#
-        # process by full image
-        dct_img = dct_fast_2d(img)
+        # process by fast DCT_2D full image
+        dct_img = dct_fast_2d(transformed_img)
         # print(dct_img)
         Image.fromarray(dct_img.copy().astype(np.uint8)).save(f'./out/dct_{filename}.png')
 
@@ -167,7 +242,7 @@ if __name__ == '__main__':
         # ======================================================#
         # process by block of image
         block = (8, 8)
-        dct_img = dct_block(img, block=block)
+        dct_img = dct_block(transformed_img, block=block)
         # print(dct_img)
 
         Image.fromarray(dct_img.copy().astype(np.uint8)).save(f'./out/dct-{block[0]}x{block[1]}_{filename}.png')
--- README.md
+++ README.md
@@ -20,11 +20,13 @@ pipenv shell # active your project venv.
 
 ## Function Introduce
 
-| Function name  |                   Parameter                    | Introdution   |
-| -------------- | :--------------------------------------------: | ------------- |
-| dct_1d()       |                 seq: np.array                  | 1D-DCT        |
-| idct_1d()      |                 seq: np.array                  | 1D-IDCT       |
-| dct_fast_2d()  |                 img: np.array                  | Fast 2D-IDCT  |
-| idct_fast_2d() |                 img: np.array                  | Fast 2D-IDCT  |
-| dct_block()    | img: np.array, block: (w, h) default is (8, 8) | 2D-DCT_block  |
-| idct_block()   | img: np.array, block: (w, h) default is (8, 8) | 2D-IDCT_block |
+| Function name  |                   Parameter                    | Introdution      |
+| -------------- | :--------------------------------------------: | ---------------- |
+| dct_2d()       |        img: np.array, dct_img: np.array        | Original 2D-DCT  |
+| idct_2d()      |     dct_img: np.array, idct_img: np.array      | Original 2D-IDCT |
+| dct_1d()       |                 seq: np.array                  | 1D-DCT           |
+| idct_1d()      |                 seq: np.array                  | 1D-IDCT          |
+| dct_fast_2d()  |                 img: np.array                  | Fast 2D-DCT      |
+| idct_fast_2d() |                 img: np.array                  | Fast 2D-IDCT     |
+| dct_block()    | img: np.array, block: (w, h) default is (8, 8) | 2D-DCT_block     |
+| idct_block()   | img: np.array, block: (w, h) default is (8, 8) | 2D-IDCT_block    |
